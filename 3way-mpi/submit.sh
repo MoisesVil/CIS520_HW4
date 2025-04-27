c17cd730b1f10c4b714458fd548969efae47062e
Synoeca <synoeca523@ksu.edu>
2025-04-27 12:09:54 -0500

    Fix issues where 3way-mpi didnt process plots

--- 3way-mpi/submit.sh
+++ 3way-mpi/submit.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=mpi_perf_%j.out
 #SBATCH --error=mpi_perf_%j.err
 
-module load CMake/3.23.1-GCCcore-11.3.0 foss/2022a
+module load CMake/3.23.1-GCCcore-11.3.0 foss/2022a OpenMPI/4.1.4-GCC-11.3.0
 
 echo "Running on host: $(hostname)"
 echo "Starting at: $(date)"
